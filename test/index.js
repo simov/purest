6487667839c2294a3ed5e8033497cf118372b2d5
simov <simeonvelichkov@gmail.com>
2013-12-09 18:13:59 +0200

    Initial commit

---
+++ test/index.js
@@ -0,0 +1,117 @@
+
+var should = require('should');
+var TinyRest = require('../lib/tinyrest');
+try {
+    var cred = require('./access-tokens');
+} catch (e) {
+    var cred = null;
+}
+
+
+describe('tinyrest', function () {
+    it('should throw an error on non specified provider', function (done) {
+        (function () {
+            var tr = new TinyRest();
+        }).should.throw('TinyRest: provider option is required!');
+        done();
+    });
+    it('should throw an error on non existing provider', function (done) {
+        (function () {
+            var tr = new TinyRest({provider:'dood'});
+        }).should.throw('TinyRest: non existing provider!');
+        done();
+    });
+    it('should create bily instance', function (done) {
+        var tr = new TinyRest({provider:'bitly'}, 'alabala');
+        tr.provider.version.should.equal('v3');
+        tr.provider.endpoint.should.equal('https://api-ssl.bitly.com');
+        done();
+    });
+    it('should create stocktwits instance', function (done) {
+        var tr = new TinyRest({provider:'stocktwits'});
+        tr.provider.version.should.equal('2');
+        tr.provider.endpoint.should.equal('https://api.stocktwits.com');
+        done();
+    });
+    it('should create linkedin instance', function (done) {
+        var tr = new TinyRest({provider:'linkedin'});
+        tr.provider.version.should.equal('v1');
+        tr.provider.endpoint.should.equal('http://api.linkedin.com');
+        done();
+    });
+    it('should create bitly path', function (done) {
+        var tr = new TinyRest({provider:'bitly'});
+        tr.provider.createPath('link/clicks')
+            .should.equal('/v3/link/clicks');
+        done();
+    });
+    it('should create stocktwits path', function (done) {
+        var tr = new TinyRest({provider:'stocktwits'});
+        tr.provider.createPath('account/verify')
+            .should.equal('/api/2/account/verify.json');
+        done();
+    });
+    it('should create linkedin path', function (done) {
+        var tr = new TinyRest({provider:'linkedin'});
+        tr.provider.createPath('people')
+            .should.equal('/v1/people');
+        done();
+    });
+    it('should create a querystring', function (done) {
+        var tr = new TinyRest({provider:'bitly'});
+        tr.toQueryString({one:'1',two:2})
+            .should.equal('one=1&two=2');
+        done();
+    });
+    it('should get bitly path', function (done) {
+        var tr = new TinyRest({provider:'bitly'});
+        tr.getPath('link/clicks',{link:'http://bitly.com/1cZKMQh'})
+            .should.equal('/v3/link/clicks?link=http://bitly.com/1cZKMQh');
+        done();
+    });
+    it('should get stocktwits path', function (done) {
+        var tr = new TinyRest({provider:'stocktwits'});
+        tr.getPath('search',{q:'stocktwits'})
+            .should.equal('/api/2/search.json?q=stocktwits');
+        done();
+    });
+    it('should get linkedin path', function (done) {
+        var tr = new TinyRest({provider:'linkedin'});
+        tr.getPath('companies',{'email-domain':'apple.com'})
+            .should.equal('/v1/companies?email-domain=apple.com');
+        done();
+    });
+    it('should get bitly resource', function (done) {
+        var tr = new TinyRest({provider:'bitly'});
+        tr.get('bitly_pro_domain', {access_token:cred.bitly,domain:'nyti.ms'}, function (err, data, res) {
+            if (err) return (err instanceof Error) ? done(err) : (console.log(err)||done(new Error('Network error!')));
+            data.data.domain.should.equal('nyti.ms');
+            data.data.bitly_pro_domain.should.equal(true);
+            done();
+        });
+    });
+    it('should get stocktwits resource', function (done) {
+        var tr = new TinyRest({provider:'stocktwits'});
+        tr.get('streams/user/StockTwits', function (err, data, res) {
+            if (err) return (err instanceof Error) ? done(err) : (console.log(err)||done(new Error('Network error!')));
+            data.response.status.should.equal(200);
+            data.messages.length.should.equal(30);
+            done();
+        });
+    });
+    it('should get linkedin resource', function (done) {
+        var tr = new TinyRest({provider:'linkedin',
+            consumerKey:cred.linkedin.consumer.key,
+            consumerSecret:cred.linkedin.consumer.secret});
+        tr.get('companies', {
+            oauth_token:cred.linkedin.user.token,
+            oauth_token_secret:cred.linkedin.user.secret,
+            'email-domain':'apple.com'
+        }, function (err, data, res) {
+            if (err) return (err instanceof Error) ? done(err) : (console.log(err)||done(new Error('Network error!')));
+            data.values[0].id.should.equal(162479);
+            data.values[0].name.should.equal('Apple');
+            done();
+        });
+    });
+});
