faf9ca9a95b3f9c050f4f410587b88c270424f84
simov <simeonvelichkov@gmail.com>
2014-02-02 14:38:12 +0200

    Improved oauth example

--- examples/oauth/app.js
+++ examples/oauth/app.js
@@ -6,15 +6,12 @@ var express = require('express'),
 var app = express();
 
 app.configure(function() {
-    // config
-
+    
     app.set('port', process.env.PORT || 3000);
     app.set('views', __dirname);
     app.set('view engine', 'html');
     // app.set('view cache', true);
     app.engine('html', consolidate.hogan);
-
-    // middleware
 
     app.use(express.cookieParser('very secret - required'));
     app.use(express.session());
@@ -31,69 +28,53 @@ app.configure('development', function() {
 });
 
 
-var passport = require('passport');
+var passport = require('passport'),
+    cred = require('../../test/credentials');
 
-var TwitterStrategy = require('passport-twitter').Strategy,
-    BitlyStrategy = require('passport-bitly').Strategy,
-    SoundcloudStrategy = require('passport-soundcloud').Strategy;
+['twitter', 'facebook', 'linkedin', 'soundcloud', 'stocktwits', 'bitly']
+.forEach(function (provider) {
+    
+    var options = {};
+    var key    = /(twitter|linkedin)/.test(provider) ? 'consumerKey'    : 'clientID';
+    var secret = /(twitter|linkedin)/.test(provider) ? 'consumerSecret' : 'clientSecret';
 
-passport.use(new TwitterStrategy({
-        consumerKey:'JcUnnvfEBZvlTqGOJxmx0w', // simo 1 app
-        consumerSecret:'deu3YCb0M3Qi6HKUVVgqBEwQ2YmhVLCwVAB4J7OM',
-        callbackURL:'http://' + 'localhost:' + app.get('port')  + '/connect/twitter/callback',
-        passReqToCallback:true
-    }, function (req, token, secret, profile, done) {
+    options[key] = cred.app[provider].key;
+    options[secret] = cred.app[provider].secret;
+    options.callbackURL = ['http://','localhost:',app.get('port'),'/connect/',provider,'/callback'].join('');
+    options.passReqToCallback = true;
+
+    var strategy = new (require('passport-'+provider).Strategy)(options, function (req, token, secret, profile, done) {
         console.log('account', '->', profile.username);
         console.log('token', '->', token);
         console.log('secret', '->', secret);
-        console.log('-------------------------------');
-        
         done();
-    }
-));
-passport.use(new BitlyStrategy({
-        clientID:'7a4862495479086bd98970535cbc3a6aa36a3029', // Kojongi app
-        clientSecret:'67f3fea668e4871c303eb4f27e12bffe6fe3fdba',
-        callbackURL:'http://' + 'localhost:' + app.get('port')  + '/connect/bitly/callback',
-        passReqToCallback:true
-    }, function (req, token, secret, profile, done) {
-        console.log('account', '->', profile);
-        console.log('token', '->', token);
-        console.log('secret', '->', secret);
-        console.log('-------------------------------');
-        
-        done();
-    }
-));
-passport.use(new SoundcloudStrategy({
-        clientID:'5f895a1c316123865dd06189837496aa', // 
-        clientSecret:'4089eb40412ee26c3521a81d2094f541',
-        callbackURL:'http://' + 'localhost:' + app.get('port')  + '/connect/soundcloud/callback',
-        passReqToCallback:true
-    }, function (req, token, secret, profile, done) {
-        console.log('account', '->', profile);
-        console.log('token', '->', token);
-        console.log('secret', '->', secret);
-        console.log('-------------------------------');
-        
-        done();
-    }
-));
+    });
+
+    passport.use(strategy);
+});
 
 app.get('/connect/twitter', passport.authorize('twitter', { failureRedirect:'/', successRedirect:'/' }));
 app.get('/connect/twitter/callback', passport.authorize('twitter', { failureRedirect:'/', successRedirect:'/' }));
 
-app.get('/connect/bitly', passport.authenticate('bitly', { failureRedirect:'/', successRedirect:'/' }));
-app.get('/connect/bitly/callback', passport.authenticate('bitly', { failureRedirect:'/', successRedirect:'/' }));
+app.get('/connect/facebook', passport.authorize('facebook', { scope: ['publish_actions', 'publish_stream', 'read_stream', 'manage_pages', 'user_groups', 'friends_groups'], failureRedirect:'/', successRedirect:'/' }));
+app.get('/connect/facebook/callback', passport.authorize('facebook', { failureRedirect:'/', successRedirect:'/' }));
+
+app.get('/connect/linkedin', passport.authorize('linkedin', { scope: ['r_basicprofile', 'r_fullprofile', 'r_emailaddress', 'r_network', 'r_contactinfo', 'rw_nus', 'rw_company_admin', 'rw_groups', 'w_messages'], failureRedirect:'/', successRedirect:'/' }));
+app.get('/connect/linkedin/callback', passport.authorize('linkedin', { failureRedirect:'/', successRedirect:'/' }));
+
+app.get('/connect/stocktwits', passport.authorize('stocktwits', { scope: ['read','watch_lists','publish_messages','publish_watch_lists','follow_users','follow_stocks'], failureRedirect:'/', successRedirect:'/' }));
+app.get('/connect/stocktwits/callback', passport.authorize('stocktwits', { failureRedirect:'/', successRedirect:'/' }));
 
 app.get('/connect/soundcloud', passport.authenticate('soundcloud', { failureRedirect:'/', successRedirect:'/' }));
 app.get('/connect/soundcloud/callback', passport.authenticate('soundcloud', { failureRedirect:'/', successRedirect:'/' }));
+
+app.get('/connect/bitly', passport.authenticate('bitly', { failureRedirect:'/', successRedirect:'/' }));
+app.get('/connect/bitly/callback', passport.authenticate('bitly', { failureRedirect:'/', successRedirect:'/' }));
 
 
 app.get('/', function (req, res) {
     res.render('app');
 });
-
 
 app.listen(app.get('port'), function() {
     console.log('Express server listening on port ' + app.get('port'));
