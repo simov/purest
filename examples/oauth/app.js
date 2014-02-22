324dce3c796244397c9009d70666a12f095f7728
simov <simeonvelichkov@gmail.com>
2014-02-22 17:38:25 +0200

    Refactored passport's provider routes

--- examples/oauth/app.js
+++ examples/oauth/app.js
@@ -59,37 +59,30 @@ var passport = require('passport'),
     passport.use(strategy);
 });
 
-app.get('/connect/twitter', passport.authorize('twitter', { failureRedirect:'/', successRedirect:'/' }));
-app.get('/connect/twitter/callback', passport.authorize('twitter', { failureRedirect:'/', successRedirect:'/' }));
 
-app.get('/connect/facebook', passport.authorize('facebook', { scope: ['publish_actions', 'publish_stream', 'read_stream', 'manage_pages', 'user_groups', 'friends_groups'], failureRedirect:'/', successRedirect:'/' }));
-app.get('/connect/facebook/callback', passport.authorize('facebook', { failureRedirect:'/', successRedirect:'/' }));
-
-app.get('/connect/linkedin', passport.authorize('linkedin', { scope: ['r_basicprofile', 'r_fullprofile', 'r_emailaddress', 'r_network', 'r_contactinfo', 'rw_nus', 'rw_company_admin', 'rw_groups', 'w_messages'], failureRedirect:'/', successRedirect:'/' }));
-app.get('/connect/linkedin/callback', passport.authorize('linkedin', { failureRedirect:'/', successRedirect:'/' }));
-
-app.get('/connect/stocktwits', passport.authorize('stocktwits', { scope: ['read','watch_lists','publish_messages','publish_watch_lists','follow_users','follow_stocks'], failureRedirect:'/', successRedirect:'/' }));
-app.get('/connect/stocktwits/callback', passport.authorize('stocktwits', { failureRedirect:'/', successRedirect:'/' }));
-
-app.get('/connect/soundcloud', passport.authenticate('soundcloud', { scope: ['non-expiring'], failureRedirect:'/', successRedirect:'/' }));
-app.get('/connect/soundcloud/callback', passport.authenticate('soundcloud', { failureRedirect:'/', successRedirect:'/' }));
-
-app.get('/connect/bitly', passport.authenticate('bitly', { failureRedirect:'/', successRedirect:'/' }));
-app.get('/connect/bitly/callback', passport.authenticate('bitly', { failureRedirect:'/', successRedirect:'/' }));
-
-app.get('/connect/github', passport.authorize('github', { scope: [], failureRedirect:'/', successRedirect:'/' }));
-app.get('/connect/github/callback', passport.authorize('github', { failureRedirect:'/', successRedirect:'/' }));
-
-app.get('/connect/stackexchange', passport.authorize('stackexchange', { scope: [], failureRedirect:'/', successRedirect:'/' }));
-app.get('/connect/stackexchange/callback', passport.authorize('stackexchange', { failureRedirect:'/', successRedirect:'/' }));
-
-app.get('/connect/google', passport.authenticate('google', {
-    scope:[
+var permissions = {
+    twitter:[],
+    facebook:['publish_actions', 'publish_stream', 'read_stream', 'manage_pages', 'user_groups', 'friends_groups'],
+    linkedin:['r_basicprofile', 'r_fullprofile', 'r_emailaddress', 'r_network', 'r_contactinfo', 'rw_nus', 'rw_company_admin', 'rw_groups', 'w_messages'],
+    stocktwits:['read','watch_lists','publish_messages','publish_watch_lists','follow_users','follow_stocks'],
+    soundcloud:['non-expiring'],
+    bitly:[],
+    github:[],
+    stackexchange:[],
+    google:[
         'https://www.googleapis.com/auth/userinfo.profile',
         'https://www.googleapis.com/auth/youtube.readonly',
         'https://www.googleapis.com/auth/drive.readonly'
-    ], failureRedirect:'/', successRedirect:'/' }));
-app.get('/connect/google/callback', passport.authenticate('google', { failureRedirect:'/', successRedirect:'/' }));
+    ]
+};
+
+for (var provider in permissions) {
+    var method = /(soundcloud|bitly)/.test(provider) ? 'authenticate' : 'authorize';
+
+    app.get('/connect/'+provider, passport[method](provider, {scope:permissions[provider], failureRedirect:'/', successRedirect:'/'}));
+    app.get('/connect/'+provider+'/callback', passport[method](provider, {failureRedirect:'/', successRedirect:'/'}));
+}
+
 
 app.get('/', function (req, res) {
     res.render('app');
