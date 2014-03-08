d59f7ba2a1c655c82f06537c3ffeecf01793ba8a
simov <simeonvelichkov@gmail.com>
2014-03-09 01:51:08 +0200

    Fixed test and twitter posting

--- test/network/post.js
+++ test/network/post.js
@@ -1,10 +1,11 @@
 
-var TinyRest = require('../lib/tinyrest'),
-    cred = require('./credentials');
+var TinyRest = require('../../lib/tinyrest'),
+    providers = require('../../config/providers'),
+    cred = require('../credentials');
 
 
 describe('post', function () {
-    var t = null;
+    var t = {};
     before(function (done) {
         for (var name in providers) {
             var provider = providers[name];
@@ -19,7 +20,7 @@ describe('post', function () {
         }
         done();
     });
-    it('should post twitter resource', function (done) {
+    it.skip('should post twitter resource', function (done) {
         t.twitter.post('statuses/update', {
             oauth:{token:cred.user.twitter.token, secret:cred.user.twitter.secret},
             form:{status:'Message on '+new Date()}
@@ -30,7 +31,7 @@ describe('post', function () {
             done();
         });
     });
-    it('should post linkedin resource', function (done) {
+    it.skip('should post linkedin resource', function (done) {
         t.linkedin.post('people/~/shares', {
             oauth:{token:cred.user.linkedin.token, secret:cred.user.linkedin.secret},
             form:{
@@ -45,7 +46,7 @@ describe('post', function () {
             done();
         });
     });
-    it('should post facebook resource', function (done) {
+    it.skip('should post facebook resource', function (done) {
         t.facebook.post('me/feed', {
             qs:{access_token: cred.user.facebook.token},
             form:{message: 'Message on '+new Date()}
@@ -56,7 +57,7 @@ describe('post', function () {
             done();
         });
     });
-    it('should post stocktwits resource', function (done) {
+    it.skip('should post stocktwits resource', function (done) {
         t.stocktwits.post('messages/create', {
             qs:{access_token: cred.user.stocktwits.token},
             form:{body: 'Message on '+new Date()}
