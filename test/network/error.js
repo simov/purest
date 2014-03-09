7b5a1b554898ea0ee5a67870314cc610cd5549eb
simov <simeonvelichkov@gmail.com>
2014-03-09 14:02:30 +0200

    Fixed error tests

--- test/network/error.js
+++ test/network/error.js
@@ -1,11 +1,28 @@
 
-var TinyRest = require('../lib/tinyrest');
+var TinyRest = require('../../lib/tinyrest'),
+    providers = require('../../config/providers'),
+    cred = require('../credentials');
 
 
-describe.skip('error', function () {
-    it('should return stocktwits api error', function (done) {
-        var t = new TinyRest({provider:'stocktwits'});
-        t.get('streams/user/nonexisting', function (err, res, body) {
+describe('error', function () {
+    var t = {};
+    before(function (done) {
+        for (var name in providers) {
+            var provider = providers[name];
+            if (provider.oauth) {
+                t[name] = new TinyRest({provider:name,
+                    consumerKey:cred.app[name].key,
+                    consumerSecret:cred.app[name].secret
+                });
+            } else {
+                t[name] = new TinyRest({provider:name});
+            }
+        }
+        done();
+    });
+
+    it.skip('should return stocktwits api error', function (done) {
+        t.stocktwits.get('streams/user/nonexisting', function (err, res, body) {
             err.response.status.should.equal(404);
             err.errors[0].message.should.equal('User not found');
             done();
