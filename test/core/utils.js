f07214b61814dd0dee745b74f4d91faa8b159aaf
simov <simeonvelichkov@gmail.com>
2014-08-15 09:50:38 +0300

    Improve utils response tests

--- test/core/utils.js
+++ test/core/utils.js
@@ -15,41 +15,96 @@ describe('utils', function () {
                 .should.equal('a=%21%2a%28%29%27&b=2');
         });
     });
+
     describe('response', function () {
-        it('don\'t throw error on missing callback', function (done) {
+        it('don\'t throw error on missing callback', function () {
             utils.response()(null, {}, {});
-            done();
         });
-        it('return on error', function (done) {
+        it('return on error', function () {
             utils.response(function (err, res, body) {
-                err.should.equal('err');
-                res.should.equal('res');
-                body.should.equal('body');
-                done();
-            })('err', 'res', 'body');
+                err.should.be.an.instanceOf(Error);
+            })(new Error());
         });
-        it('try to parse body string as JSON', function (done) {
-            utils.response(function (err, res, body) {
-                if (err) return done(err);
-                should.deepEqual(body, {data:'data'});
-                done();
-            })(
-                null,
-                {statusCode:200,headers:{'content-encoding':'application/json'}},
-                '{"data":"data"}'
-            );
+
+        describe('parse string response', function () {
+            it('content-encoding: application/json', function (done) {
+                utils.response(function (err, res, body) {
+                    if (err) return done(err);
+                    should.deepEqual(body, {data:'data'});
+                    done();
+                })(
+                    null,
+                    {statusCode:200,headers:{'content-encoding':'application/json'}},
+                    '{"data":"data"}'
+                );
+            });
+            it('content-type: text/javascript', function (done) {
+                utils.response(function (err, res, body) {
+                    if (err) return done(err);
+                    should.deepEqual(body, {data:'data'});
+                    done();
+                })(
+                    null,
+                    {statusCode:200,headers:{'content-type':'text/javascript'}},
+                    '{"data":"data"}'
+                );
+            });
+            it('content-type: application/json', function (done) {
+                utils.response(function (err, res, body) {
+                    if (err) return done(err);
+                    should.deepEqual(body, {data:'data'});
+                    done();
+                })(
+                    null,
+                    {statusCode:200,headers:{'content-type':'application/json'}},
+                    '{"data":"data"}'
+                );
+            });
+            it('handle flickr response', function (done) {
+                utils.response(function (err, res, body) {
+                    if (err) return done(err);
+                    should.deepEqual(body, {data:'data'});
+                    done();
+                })(
+                    null,
+                    {statusCode:200,headers:{'content-type':'text/javascript'}},
+                    'jsonFlickrApi({"data":"data"})'
+                );
+            });
+            it('return parse error on malformed json', function (done) {
+                utils.response(function (err, res, body) {
+                    err.message.should.equal('Parse error!')
+                    body.should.equal('<html>');
+                    done();
+                })(
+                    null,
+                    {statusCode:200,headers:{'content-encoding':'application/json'}},
+                    '<html>'
+                );
+            });
         });
-        it('return parse error on malformed json', function (done) {
-            utils.response(function (err, res, body) {
-                err.message.should.equal('Parse error!')
-                body.should.equal('<html>');
-                done();
-            })(
-                null,
-                {statusCode:200,headers:{'content-encoding':'application/json'}},
-                '<html>'
-            );
+
+        describe('decompress', function () {
+            it('gzip encoded body', function (done) {
+                zlib.gzip('{"data":"data"}', function (err, encoded) {
+                    utils.response(function (err, res, body) {
+                        if (err) return done(err);
+                        should.deepEqual(body, {data:'data'});
+                        done();
+                    })(null, {statusCode:200,headers:{'content-encoding':'gzip'}}, encoded);
+                });
+            });
+            it('deflate encoded body', function (done) {
+                zlib.deflate('{"data":"data"}', function (err, encoded) {
+                    utils.response(function (err, res, body) {
+                        if (err) return done(err);
+                        should.deepEqual(body, {data:'data'});
+                        done();
+                    })(null, {statusCode:200,headers:{'content-encoding':'deflate'}}, encoded);
+                });
+            });
         });
+
         it('return error on non successful status code', function (done) {
             utils.response(function (err, res, body) {
                 should.deepEqual(err, {data:'data'});
@@ -67,24 +122,6 @@ describe('utils', function () {
                 should.deepEqual(body, {data:'data'});
                 done();
             })(null, {statusCode:200,headers:{}}, {data:'data'});
-        });
-        it('decompress a gzip encoded body', function (done) {
-            zlib.gzip('{"data":"data"}', function (err, encoded) {
-                utils.response(function (err, res, body) {
-                    if (err) return done(err);
-                    should.deepEqual(body, {data:'data'});
-                    done();
-                })(null, {statusCode:200,headers:{'content-encoding':'gzip'}}, encoded);
-            });
-        });
-        it('decompress a deflate encoded body', function (done) {
-            zlib.deflate('{"data":"data"}', function (err, encoded) {
-                utils.response(function (err, res, body) {
-                    if (err) return done(err);
-                    should.deepEqual(body, {data:'data'});
-                    done();
-                })(null, {statusCode:200,headers:{'content-encoding':'deflate'}}, encoded);
-            });
         });
     });
 });
