ef98d05eb85c3c284183ff0b4c227405e174d33e
simov <simeonvelichkov@gmail.com>
2015-08-03 10:16:00 +0300

    Fix request tests

--- test/request/refresh.js
+++ test/request/refresh.js
@@ -23,7 +23,7 @@ for (var name in providers) {
     provider:name,
     defaults:{headers:{'User-Agent':'Purest'}}
   }
-  if (providers[name].__provider && providers[name].__provider.oauth) {
+  if (app[name] && user[name] && user[name].token && user[name].secret) {
     options.key = app[name].key
     options.secret = app[name].secret
   }
