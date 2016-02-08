306e3ab41a1e58b403aa5aeb2405105ba61b5871
simov <simeonvelichkov@gmail.com>
2016-02-08 16:03:30 +0200

    Add auth custom method + Add oauth config helper method + Set parse.json to true by default

--- index.js
+++ index.js
@@ -41,18 +41,55 @@ module.exports = ((client, api, config, extend) => {
     self.version = options.version
     self.type = options.type
 
-
     var url = config.url(self)
+    var oauth = config.oauth(self)
+
+
+    // oauth
+    if (options.key) {
+      self.key = options.key
+    }
+    if (options.secret) {
+      self.secret = options.secret
+    }
 
 
     var _config = {
-      verb: {get: ['select']},
-      option: {qs: ['where'], callback: []},
-      custom: {submit: [], api: ['query']}
+      method: {get: ['select']},
+      option: {qs: ['where'], parse: [], callback: []},
+      custom: {submit: [], api: ['query'], auth: []}
     }
     var _custom = {
       api: function (options, name) {
         options.api = name
+        return this
+      },
+      auth: function (options, a, b) {
+        var cfg = self.aliases[options.api || self.api || '__default']
+
+        var auth = config.endpoint.auth(options.url, cfg.endpoints) || cfg.auth
+
+        if (auth) {
+          if (auth instanceof Array) {
+            auth = (a && b) ? auth[1] : auth[0]
+          }
+
+          var result = {}
+          Object.keys(auth).forEach(function (key) {
+            result[key] = {}
+            Object.keys(auth[key]).forEach(function (sub) {
+              if (auth[key][sub] === '[0]') {
+                result[key][sub] = a
+              }
+              else if (auth[key][sub] === '[1]') {
+                result[key][sub] = b
+              }
+            })
+          })
+
+          extend(true, options, result)
+        }
+
         return this
       },
       submit: function (options) {
@@ -60,6 +97,17 @@ module.exports = ((client, api, config, extend) => {
         var cfg = self.aliases[options.api || self.api || '__default']
         if (!cfg) {
           throw new Error('Purest: non existing API!')
+        }
+
+        // oauth
+        oauth(options)
+
+        // json
+        if (options.parse === undefined) {
+          options.parse = {json: true}
+        }
+        else if (options.parse.json === undefined) {
+          options.parse.json = true
         }
 
         // create absolute url
