ba4484447752f9e611ef6e5d284085f4c62d72d1
simov <simeonvelichkov@gmail.com>
2016-02-10 12:54:57 +0200

    Major refactoring

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 
-module.exports = require('./lib/purest')(
-  require('@request/client'),
-  require('@request/api'),
-  require('@purest/config'),
-  require('extend')
-)
+var client = require('@request/client')
+
+var _provider = require('./lib/provider')
+var _config = require('./lib/config')
+var _options = require('./lib/options')
+var _methods = require('./lib/methods')
+var _purest = require('./lib/client')
+
+
+module.exports = (options) => {
+  var provider = _provider(options)
+  var config = _config(provider, options)
+  var transform = _options(provider)
+
+  var methods = _methods(options)
+
+  return _purest(client, provider, methods, config, transform)
+}
