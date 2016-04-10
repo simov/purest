a87a4d0f8cebab45c375ec369814c6bbb058975f
simov <simeonvelichkov@gmail.com>
2016-04-10 20:32:17 +0300

    Use arrow closure

--- index.js
+++ index.js
@@ -6,47 +6,45 @@ var _options = require('./lib/options')
 var _purest = require('./lib/client')
 
 
-module.exports = (client) => {
-  return (options) => {
-    var provider = {
-      api: options.api,
-      subdomain: options.subdomain,
-      subpath: options.subpath,
-      version: options.version,
-      type: options.type,
-      key: options.key,
-      secret: options.secret
-    }
+module.exports = (client) => ((options) => {
+  var provider = {
+    api: options.api,
+    subdomain: options.subdomain,
+    subpath: options.subpath,
+    version: options.version,
+    type: options.type,
+    key: options.key,
+    secret: options.secret
+  }
 
-    var config
+  var config
 
-    if (!options || !options.provider) {
-      throw new Error('Purest: provider option is required!')
-    }
+  if (!options || !options.provider) {
+    throw new Error('Purest: provider option is required!')
+  }
 
-    if (options.config) {
-      if (options.config[options.provider]) {
-        config = _config(provider, options.config[options.provider])
-      }
-      else {
-        throw new Error('Purest: non existing provider!')
-      }
+  if (options.config) {
+    if (options.config[options.provider]) {
+      config = _config(provider, options.config[options.provider])
     }
     else {
-      throw new Error('Purest: config option is required!')
+      throw new Error('Purest: non existing provider!')
     }
-
-    if (options.api) {
-      if (!config.aliases || !config.aliases[options.api]) {
-        throw new Error('Purest: non existing alias!')
-      }
-    }
-
-    var methods = {}
-    extend(true, methods, _methods, options.methods)
-
-    var transform = _options(provider)
-
-    return _purest(client, provider, methods, config, transform)
   }
-}
+  else {
+    throw new Error('Purest: config option is required!')
+  }
+
+  if (options.api) {
+    if (!config.aliases || !config.aliases[options.api]) {
+      throw new Error('Purest: non existing alias!')
+    }
+  }
+
+  var methods = {}
+  extend(true, methods, _methods, options.methods)
+
+  var transform = _options(provider)
+
+  return _purest(client, provider, methods, config, transform)
+})
