74ddd5a7c482c02c15b3002a10c6ecb388b27f56
simov <simeonvelichkov@gmail.com>
2016-04-14 16:00:28 +0300

    Move the basic api tests into separate file

---
+++ test/api.js
@@ -0,0 +1,90 @@
+
+var t = require('assert')
+var http = require('http')
+var client = require('@request/client')
+var purest = require('../')(client)
+
+
+describe('basic', () => {
+  var provider = purest({basic: true, provider: 'purest', config: {purest: {
+    'http://localhost:6767': {
+      'api/{endpoint}': {
+        __path: {alias: '__default'}
+      }
+    }
+  }}})
+
+  describe('function', () => {
+    var server
+
+    before((done) => {
+      server = http.createServer()
+      server.on('request', (req, res) => {
+        res.end(req.url)
+      })
+      server.listen(6767, done)
+    })
+
+    it('url, options, callback', (done) => {
+      provider('user/profile', {qs: {a: 1}}, (err, res, body) => {
+        t.equal(body, '/api/user/profile?a=1')
+        done()
+      })
+    })
+
+    it('url, options', (done) => {
+      provider('user/profile', {qs: {a: 1}, callback: (err, res, body) => {
+        t.equal(body, '/api/user/profile?a=1')
+        done()
+      }})
+    })
+
+    it('url, callback', (done) => {
+      provider('user/profile', (err, res, body) => {
+        t.equal(body, '/api/user/profile')
+        done()
+      })
+    })
+
+    it('options, callback', (done) => {
+      provider({url: 'user/profile', qs: {a: 1}}, (err, res, body) => {
+        t.equal(body, '/api/user/profile?a=1')
+        done()
+      })
+    })
+
+    it('options', (done) => {
+      provider({url: 'user/profile', qs: {a: 1}, callback: (err, res, body) => {
+        t.equal(body, '/api/user/profile?a=1')
+        done()
+      }})
+    })
+
+    after((done) => {
+      server.close(done)
+    })
+  })
+
+  describe('HTTP verbs', () => {
+    var server
+
+    before((done) => {
+      server = http.createServer()
+      server.on('request', (req, res) => {
+        res.end(req.method + ' ' + req.url)
+      })
+      server.listen(6767, done)
+    })
+
+    it('url, options, callback', (done) => {
+      provider.post('user/profile', {qs: {a: 1}}, (err, res, body) => {
+        t.equal(body, 'POST /api/user/profile?a=1')
+        done()
+      })
+    })
+
+    after((done) => {
+      server.close(done)
+    })
+  })
+})
