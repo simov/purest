ebaf085219253d74ae020da992040faebccaa779
simov <simeonvelichkov@gmail.com>
2016-04-15 12:28:33 +0300

    Add callback wrapper + Parse json by default when using the legacy request module

--- test/request.js
+++ test/request.js
@@ -2,10 +2,12 @@
 var t = require('assert')
 var http = require('http')
 var client = require('@request/client')
-var purest = require('../')(client)
+var request = require('request')
+var Purest = require('../')
 
 
-describe('request', () => {
+describe('before hook', () => {
+  var purest = Purest(client)
   var server
 
   before((done) => {
@@ -35,6 +37,59 @@ describe('request', () => {
       .select('me')
       .request((err, res, body) => {
         t.equal(body, '/api/me?a=1')
+        done()
+      })
+  })
+
+  after((done) => {
+    server.close(done)
+  })
+})
+
+describe('parse JSON by default', () => {
+  var server
+
+  before((done) => {
+    server = http.createServer()
+    server.on('request', (req, res) => {
+      res.writeHead(202)
+      res.end(JSON.stringify({a: 1}))
+    })
+    server.listen(6767, done)
+  })
+
+  it('request next', (done) => {
+    var purest = Purest(client)
+    var provider = purest({provider: 'purest',
+    config: {purest: {
+      'http://localhost:6767': {
+        'api/{endpoint}': {
+          __path: {alias: '__default'}
+        }
+      }
+    }}})
+    provider
+      .select('me')
+      .request((err, res, body) => {
+        t.deepEqual(body, {a: 1})
+        done()
+      })
+  })
+
+  it('request legacy', (done) => {
+    var purest = Purest(request)
+    var provider = purest({provider: 'purest',
+    config: {purest: {
+      'http://localhost:6767': {
+        'api/{endpoint}': {
+          __path: {alias: '__default'}
+        }
+      }
+    }}})
+    provider
+      .select('me')
+      .request((err, res, body) => {
+        t.deepEqual(body, {a: 1})
         done()
       })
   })
