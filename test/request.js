019bc493faf7fb51fdb1a643207937621971ceb2
simov <simeonvelichkov@gmail.com>
2016-04-23 19:27:29 +0300

    Forward purest error on request

--- test/request.js
+++ test/request.js
@@ -6,6 +6,99 @@ var request = require('request')
 var Purest = require('../')
 
 
+describe('request', () => {
+  describe('throw', () => {
+    it('callback', (done) => {
+      var purest = Purest(client)
+      var provider = purest({provider: 'purest',
+      config: {purest: {
+        'http://localhost:6767': {
+          'api/{endpoint}': {
+            __path: {alias: '__default'}
+          }
+        }
+      }}})
+      provider.query('test')
+        .select('me')
+        .request((err, res, body) => {
+          t.equal(err.message, 'Purest: non existing alias!')
+          done()
+        })
+    })
+    it('promise', (done) => {
+      var purest = Purest(client, Promise)
+      var provider = purest({provider: 'purest',
+      config: {purest: {
+        'http://localhost:6767': {
+          'api/{endpoint}': {
+            __path: {alias: '__default'}
+          }
+        }
+      }}})
+      provider.query('test')
+        .select('me')
+        .request()
+        .catch((err) => {
+          t.equal(err.message, 'Purest: non existing alias!')
+          done()
+        })
+    })
+  })
+
+  describe('parse JSON by default', () => {
+    var server
+
+    before((done) => {
+      server = http.createServer()
+      server.on('request', (req, res) => {
+        res.writeHead(202)
+        res.end(JSON.stringify({a: 1}))
+      })
+      server.listen(6767, done)
+    })
+
+    it('request next', (done) => {
+      var purest = Purest(client)
+      var provider = purest({provider: 'purest',
+      config: {purest: {
+        'http://localhost:6767': {
+          'api/{endpoint}': {
+            __path: {alias: '__default'}
+          }
+        }
+      }}})
+      provider
+        .select('me')
+        .request((err, res, body) => {
+          t.deepEqual(body, {a: 1})
+          done()
+        })
+    })
+
+    it('request legacy', (done) => {
+      var purest = Purest(request)
+      var provider = purest({provider: 'purest',
+      config: {purest: {
+        'http://localhost:6767': {
+          'api/{endpoint}': {
+            __path: {alias: '__default'}
+          }
+        }
+      }}})
+      provider
+        .select('me')
+        .request((err, res, body) => {
+          t.deepEqual(body, {a: 1})
+          done()
+        })
+    })
+
+    after((done) => {
+      server.close(done)
+    })
+  })
+})
+
 describe.skip('before hook', () => {
   var purest = Purest(client)
   var server
@@ -37,59 +130,6 @@ describe.skip('before hook', () => {
       .select('me')
       .request((err, res, body) => {
         t.equal(body, '/api/me?a=1')
-        done()
-      })
-  })
-
-  after((done) => {
-    server.close(done)
-  })
-})
-
-describe('parse JSON by default', () => {
-  var server
-
-  before((done) => {
-    server = http.createServer()
-    server.on('request', (req, res) => {
-      res.writeHead(202)
-      res.end(JSON.stringify({a: 1}))
-    })
-    server.listen(6767, done)
-  })
-
-  it('request next', (done) => {
-    var purest = Purest(client)
-    var provider = purest({provider: 'purest',
-    config: {purest: {
-      'http://localhost:6767': {
-        'api/{endpoint}': {
-          __path: {alias: '__default'}
-        }
-      }
-    }}})
-    provider
-      .select('me')
-      .request((err, res, body) => {
-        t.deepEqual(body, {a: 1})
-        done()
-      })
-  })
-
-  it('request legacy', (done) => {
-    var purest = Purest(request)
-    var provider = purest({provider: 'purest',
-    config: {purest: {
-      'http://localhost:6767': {
-        'api/{endpoint}': {
-          __path: {alias: '__default'}
-        }
-      }
-    }}})
-    provider
-      .select('me')
-      .request((err, res, body) => {
-        t.deepEqual(body, {a: 1})
         done()
       })
   })
